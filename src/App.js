c4bf298b31f3685b5c067e68b3164a0d96759ae6
kkbughunter <karthikeyan.aas@gmail.com>
2024-04-27 23:45:02 +0530

    portfolio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Iam Karthikeyan, CSE SSN College of Engineering.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
